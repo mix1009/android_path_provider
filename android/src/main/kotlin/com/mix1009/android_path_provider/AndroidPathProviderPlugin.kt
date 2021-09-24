29a68eaa3bd3ad76a287c619271b68510cc35a20
zezo <zezohassam@gmail.com>
2021-09-24 09:50:29 +0200

    migrated to v2 and android x

--- android/src/main/kotlin/com/mix1009/android_path_provider/AndroidPathProviderPlugin.kt
+++ android/src/main/kotlin/com/mix1009/android_path_provider/AndroidPathProviderPlugin.kt
@@ -1,25 +1,31 @@
 package com.mix1009.android_path_provider
 
+
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import android.os.Environment;
 
-class AndroidPathProviderPlugin: MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "android_path_provider")
-      channel.setMethodCallHandler(AndroidPathProviderPlugin())
-    }
-  }
+/** AndroidPathProviderPlugin */
+class AndroidPathProviderPlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "android_path_provider")
+    channel.setMethodCallHandler(this)
+  }
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getAlarmsPath") {
       result.success(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_ALARMS).getAbsolutePath());
-    // } else if (call.method == "getAudiobooksPath") {
+      // } else if (call.method == "getAudiobooksPath") {
       // result.success(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_AUDIOBOOKS).getAbsolutePath());
     } else if (call.method == "getDCIMPath") {
       result.success(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath());
@@ -39,10 +45,15 @@ class AndroidPathProviderPlugin: MethodCallHandler {
       result.success(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PODCASTS).getAbsolutePath());
     } else if (call.method == "getRingtonesPath") {
       result.success(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_RINGTONES).getAbsolutePath());
-    // } else if (call.method == "getScreenshotsPath") {
+      // } else if (call.method == "getScreenshotsPath") {
       // result.success(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_SCREENSHOTS).getAbsolutePath());
     } else {
       result.notImplemented()
     }
+  }
+
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
   }
 }
